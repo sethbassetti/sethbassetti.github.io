5c714390f0a8ab570362e09602bd8052e80f4a01
sethbassetti <sethbassetti@gmail.com>
2025-03-01 14:34:04 -0700

    Added dark mode and finished header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    svg: true,
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  
+  
+});
